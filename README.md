756a4535992840c7864941af345984ca72d561f5
Lucas Sansberro <lucas_sansberro@outlook.com>
2022-11-24 15:40:27 -0300

    Readme updated with a gif

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Context and redux practice using a counter and managing global props
+
+![Context API practice gif](/Pics/contextPractice.gif)
--- src/CounterText.jsx
+++ src/CounterText.jsx
@@ -4,7 +4,7 @@ import { CounterContext } from "./Context/Context";
 const CounterText = () => {
   const { counter } = useContext(CounterContext);
 
-  return <h3 className="text-center mt-5">El contador está en {counter}</h3>;
+  return <h3 className="text-center mt-5">The counter is at {counter}</h3>;
 };
 
 export default CounterText;
